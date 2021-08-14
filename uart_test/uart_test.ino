5b2e145988763b7293938f24d4b6e2db42c26e0e
Karl Ljungkvist <k.ljungkvist@gmail.com>
2021-08-14 23:34:05 +0200

    add function for blocking receive of a char

--- uart_test/uart_test.ino
+++ uart_test/uart_test.ino
@@ -70,6 +70,11 @@ void send_char(char ch) {
     write_reg(0, ch);
 }
 
+char recv_char() {
+    while( (read_reg(0) & 0x2) != 0 );
+    return read_reg(1);
+}
+
 const char charr[] = {
     'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
     'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
