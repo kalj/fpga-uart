6d3a0855811a8aacd2e9a609287236ad27b0a51f
Karl Ljungkvist <k.ljungkvist@gmail.com>
2021-08-20 22:43:20 +0200

    clean up

--- uart_test/uart_test.ino
+++ uart_test/uart_test.ino
@@ -133,21 +133,23 @@ void loop() {
     }
 }
 
-/* const char charr[] = { */
-/*     'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', */
-/*     'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', */
-/*     '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'}; */
-/* uint8_t ch = 'A'; */
+/*
+const char charr[] = {
+    'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+    'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+    '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
+uint8_t ch = 'A';
 
-/* void loop() { */
+void loop() {
 
-/*     for(int i=0; i<sizeof(charr); i++) { */
-/*         send_char(charr[i]); */
-/*     } */
-/*     send_char('\n'); */
+    for(int i=0; i<sizeof(charr); i++) {
+        send_char(charr[i]);
+    }
+    send_char('\n');
 
-/*     /\* char ch = recv_char(); *\/ */
-/*     /\* Serial.print("received:  "); *\/ */
-/*     /\* Serial.println(ch); *\/ */
-/*     delay(100); */
-/* } */
+    // char ch = recv_char();
+    // Serial.print("received:  ");
+    // Serial.println(ch);
+    delay(100);
+}
+*/
